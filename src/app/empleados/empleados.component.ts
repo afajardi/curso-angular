79affc40fff4df4ffa5c750d7c524afc67f0f40d
afajardi <fmangelete@hotmail.com>
2021-05-16 23:00:49 +0200

    Codigo hasta la clase Tema 5-26

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {Empleado} from './empleado';
 
 @Component({
   selector: 'empleados',
   templateUrl: './empleados.component.html',
   styleUrls: ['./empleados.component.css']
 })
-export class EmpleadosComponent  {
+export class EmpleadosComponent {
 
-  titulo_componente = 'Componente empleados'
+  public titulo_componente:string = 'Componente empleados';
+  public empleado: Empleado;
+  public trabajadores:Array<Empleado>;
+  public trabajador_externo:boolean;
+
+  public color:string;
+  public color_seleccionado:string;
+
+  constructor(){
+    this.empleado = new Empleado('Vicor Roblles',45,"Cocinero",true);
+    this.trabajadores = [
+      new Empleado('Manolo Martinez',35,"Cocinero",false),
+      new Empleado('Ana Lopez',25,"Programadora",true),
+      new Empleado('Vicor Roblles',45,"Costurera",false)
+    ];
+
+    this.trabajador_externo = true;
+    this.color = 'red';
+    this.color_seleccionado='#ccc'
+  
+  }
+
+  ngOnInit(){
+    
+    console.log(this.empleado);
+    console.log(this.trabajadores);
+  }
+
+  cambiarExterno(valor:boolean){
+    this.trabajador_externo = valor;
+  }
+
+  logColorSeleccionado(){
+    console.log("Color seleccionado: "+this.color_seleccionado)
+  }
 
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrutaComponent {
 
+  public nombre:string = 'Angel Fajardo';
+
   public trabajos:Array<any> = ['Bombero',44,'Fontanero','Camarero',true];
+
+  constructor(){
+    console.log(this.trabajos);
+  }
+
+  ngOnInit(){
+    this.cambiarNombre();
+    console.log("Nombre: " + this.nombre);
+
+    // Variables y alcance
+
+    var uno = 8;
+    var dos = 15;
+    console.log("ANTES DEL IF: " + uno);
+    if(uno === 8){
+      let uno = 3;
+      var dos = 88;
+      console.log("DENTRO DEL IF: " + uno + " - VAR DOS: " + dos) ;
+    }
+
+    console.log("DESPUES DEL IF: " + uno + " - VAR DOS: " + dos);
+
+  }
+
+  cambiarNombre(){
+    this.nombre = 'Victor Robles';
+  }
+
+
+  
 
 }
